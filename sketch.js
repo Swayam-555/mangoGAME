4179cb183ed793387f813c1a320956446230212c
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-04-18 16:18:49 +0530

    game

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango
 var world,boy;
 
 //Declare launcherObject and launchForce variable here
-
+ var launcherObj;
+ var launchForce = 100;
 
 function preload(){
 	boy=loadImage("images/boy.png");
@@ -40,7 +41,7 @@ function setup() {
 	treeObj=new tree(1050,580);
 	groundObject=new ground(width/2,600,width,20);
   //create launcherObject here
-
+ launcherObj=new launcher(stoneObj.body,{x:235,y:420});
 
 	Engine.run(engine);
 }
@@ -70,7 +71,7 @@ function draw() {
   stoneObj.display();
   groundObject.display();
   // display launcher object here
-    
+    launcherObj.display();
 
 
   detectollision(stoneObj,mango1);
@@ -89,12 +90,23 @@ function draw() {
 
 //create mouseDragged function here
 
+function mouseDragged(){
+  Matter.Body.setPosition(stoneObj.body,{x:235,y:420});
+}
+
 
 //create mouseReleased function here
-
+function mouseReleased(){
+  launcherObj.fly();
+}
 
 //create keyPressed function here
-
+function keyPressed(){
+  if(keyCode === 32){
+    Matter.Body.setPosition(stoneObj.body,{x:235,y:420});
+  launcherObj.attach(stoneObj.body)
+  }
+}
 
   function detectollision(lstone,lmango){
 
